bb299b7dfb615db156826ee8ca2270586aa6a1d7
Tsumari <lucasflicky@hotmail.com>
2022-01-15 00:28:21 -0300

    create game

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
-import express from 'express';
-import { setTimeout } from "timers/promises";
-import API from './api/_endpoints.js'
+const express = require('express');
+const { setTimeout } = require("timers/promises");
+const API = require('./api/_endpoints.js');
 
 const app = express();
 const port = process.env.PORT || 3001;
