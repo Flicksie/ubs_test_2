e7e5ba2605d409323cf87e11a052ee569b520a6b
Tsumari <lucasflicky@hotmail.com>
2022-01-15 00:29:25 -0300

    create endpoints centralizer

---
+++ server/api/_endpoints.js
@@ -0,0 +1,12 @@
+
+
+const Router = require("express");
+const Games = require("./games.js");
+const IGDB = require("./_authenticateIGDB.js");
+
+
+const router = Router();
+
+router.use("/games", IGDB, (...args) => Games(...args) );
+
+module.exports = router;
